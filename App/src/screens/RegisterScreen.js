ccb1add2311e3b514f4a5f625b66dfcd76de8994
dynablast <immanuelfrancis2021@gmail.com>
2025-01-31 11:03:46 +0530

    Add files via upload

---
+++ App/src/screens/RegisterScreen.js
@@ -0,0 +1,145 @@
+import React, { useState } from 'react'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text } from 'react-native-paper'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import TextInput from '../components/TextInput'
+import BackButton from '../components/BackButton'
+import { theme } from '../core/theme'
+import { emailValidator } from '../helpers/emailValidator'
+import { passwordValidator } from '../helpers/passwordValidator'
+import { nameValidator } from '../helpers/nameValidator'
+
+export default function RegisterScreen({ navigation }) {
+  const [firstName, setFirstName] = useState({ value: '', error: '' })
+  const [lastName, setLastName] = useState({ value: '', error: '' })
+  const [dob, setDob] = useState({ value: '', error: '' })
+  const [bloodGroup, setBloodGroup] = useState({ value: '', error: '' })
+  const [sex, setSex] = useState({ value: '', error: '' })
+  const [address, setAddress] = useState({ value: '', error: '' })
+  const [email, setEmail] = useState({ value: '', error: '' })
+  const [password, setPassword] = useState({ value: '', error: '' })
+
+  const onSignUpPressed = () => {
+    const firstNameError = nameValidator(firstName.value)
+    const lastNameError = nameValidator(lastName.value)
+    const emailError = emailValidator(email.value)
+    const passwordError = passwordValidator(password.value)
+    
+    if (firstNameError || lastNameError || emailError || passwordError) {
+      setFirstName({ ...firstName, error: firstNameError })
+      setLastName({ ...lastName, error: lastNameError })
+      setEmail({ ...email, error: emailError })
+      setPassword({ ...password, error: passwordError })
+      return
+    }
+    
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Dashboard' }],
+    })
+  }
+
+  return (
+    <Background>
+      <BackButton goBack={navigation.goBack} />
+      <Logo />
+      <Header>Create Account</Header>
+      <TextInput
+        label="First Name"
+        returnKeyType="next"
+        value={firstName.value}
+        onChangeText={(text) => setFirstName({ value: text, error: '' })}
+        error={!!firstName.error}
+        errorText={firstName.error}
+      />
+      <TextInput
+        label="Last Name"
+        returnKeyType="next"
+        value={lastName.value}
+        onChangeText={(text) => setLastName({ value: text, error: '' })}
+        error={!!lastName.error}
+        errorText={lastName.error}
+      />
+      <TextInput
+        label="Date of Birth (YYYY-MM-DD)"
+        returnKeyType="next"
+        value={dob.value}
+        onChangeText={(text) => setDob({ value: text, error: '' })}
+        error={!!dob.error}
+        errorText={dob.error}
+      />
+      <TextInput
+        label="Blood Group"
+        returnKeyType="next"
+        value={bloodGroup.value}
+        onChangeText={(text) => setBloodGroup({ value: text, error: '' })}
+        error={!!bloodGroup.error}
+        errorText={bloodGroup.error}
+      />
+      <TextInput
+        label="Sex (Male/Female/Other)"
+        returnKeyType="next"
+        value={sex.value}
+        onChangeText={(text) => setSex({ value: text, error: '' })}
+        error={!!sex.error}
+        errorText={sex.error}
+      />
+      <TextInput
+        label="Address"
+        returnKeyType="next"
+        value={address.value}
+        onChangeText={(text) => setAddress({ value: text, error: '' })}
+        error={!!address.error}
+        errorText={address.error}
+      />
+      <TextInput
+        label="Email"
+        returnKeyType="next"
+        value={email.value}
+        onChangeText={(text) => setEmail({ value: text, error: '' })}
+        error={!!email.error}
+        errorText={email.error}
+        autoCapitalize="none"
+        autoCompleteType="email"
+        textContentType="emailAddress"
+        keyboardType="email-address"
+      />
+      <TextInput
+        label="Password"
+        returnKeyType="done"
+        value={password.value}
+        onChangeText={(text) => setPassword({ value: text, error: '' })}
+        error={!!password.error}
+        errorText={password.error}
+        secureTextEntry
+      />
+      <Button
+        mode="contained"
+        onPress={onSignUpPressed}
+        style={{ marginTop: 24 }}
+      >
+        Sign Up
+      </Button>
+      <View style={styles.row}>
+        <Text>Already have an account? </Text>
+        <TouchableOpacity onPress={() => navigation.replace('LoginScreen')}>
+          <Text style={styles.link}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </Background>
+  )
+}
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    marginTop: 4,
+  },
+  link: {
+    fontWeight: 'bold',
+    color: theme.colors.primary,
+  },
+})
